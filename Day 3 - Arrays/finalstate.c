7c015cbc549327a7fb12eeb7607d31cbbc637258
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-12-23 11:51:33 +0530

    Update finalstate.c

--- Day 3 - Arrays/finalstate.c
+++ Day 3 - Arrays/finalstate.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 int main()
 {
-    int n=0,a=0,b=0,x=0,temp=0;
+    int n=0,temp=0;
     printf("Enter no. of elements: ");
     scanf("%d",&n);
-    x=n;
     int nums[n];
     printf("Enter the array elements:");
     for(int i=0;i<n;i++)
@@ -18,8 +16,6 @@ int main()
             printf("False");
             return 0;
         }
-        if(i==0 && nums[i]==1) 
-            i=i+nums[temp]-2;
         i=i+nums[temp]-1;
         if(i==n-1){
             printf("True");
@@ -29,3 +25,24 @@ int main()
     printf("False");
     return 0;
 }
+
+
+/*
+Input:
+Enter no. of elements: 5
+Enter the array elements:2 3 1 1 4
+Output:
+True
+
+Input:
+Enter no. of elements: 4
+Enter the array elements:1 2 1 0
+Output:
+True
+
+Input:
+Enter no. of elements: 4
+Enter the array elements:1 2 5 0
+Output:
+False
+*/
